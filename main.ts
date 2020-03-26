b1b8ca476130a7aca749c12d15eb5ba64b0160b4
NotBlizzard <furryblizzard@gmail.com>
2020-03-25 23:17:41 -0400

    disable helmet.

--- main.ts
+++ main.ts
@@ -5,19 +5,18 @@ import cookie from "cookie-parser";
 import express from "express";
 import expressHandlebars from "express-handlebars";
 import session from "express-session";
-import helmet from "helmet";
+//import helmet from "helmet";
 import http from "http";
 import redis from "redis";
 import flash from "express-flash";
 import socketio from "socket.io";
 import cors from "cors";
 import passport from "./authentication";
-import db from "./db";
 import router from "./routes";
 import websocket from "./websocket";
+import db from "./db";
 
 const app = express();
-db();
 
 const server = http.createServer(app);
 const client = redis.createClient(process.env.REDIS_URL || "");
@@ -27,7 +26,7 @@ const RedisStore = redisStore(session);
 app.engine("handlebars", expressHandlebars());
 app.set("views", "views");
 app.set("view engine", "handlebars");
-app.use(helmet());
+//app.use(helmet());
 app.use(cors());
 app.use(flash());
 app.use("/", express.static("dist"));
@@ -49,6 +48,7 @@ app.use("/", router);
 
 const io = socketio(server);
 websocket(io);
+db();
 
 server.listen(process.env.PORT || 8000, () =>
   console.log(`Listening on port ${process.env.PORT || 8000}`),
